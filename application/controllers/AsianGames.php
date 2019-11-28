9e6316b47338dd3969c8e6db4591fd36aab9a36c
danny <47197859+dannylukmana@users.noreply.github.com>
2019-11-29 00:15:00 +0700

    fix CI

--- application/controllers/AsianGames.php
+++ application/controllers/AsianGames.php
@@ -20,61 +20,53 @@ class AsianGames extends CI_Controller
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
-	function __construct()
+	public function __construct()
 	{
-        parent::__construct();
-        $this->load->helper('url');
+		parent::__construct();
+		
+		$this->load->helper('url');
 		$this->load->library('session');
 		$this->load->library('form_validation');
-	}
-	public function index()
-	{
-		$this->load->view('homepage');
     }
 
-    public function viewcabor()
-	{
-		$this->load->view('viewCabor');
-	}
-
-	public function addAtlet()
-	{
-		
-		$this->load->view('insertAtlet');
-	}
-	
+    public function index(){
+        $this->load->view('homepage');
+    }
     
     public function login()
-	{	echo "lalalalalalaa";	
+	{	
+		$this->form_validation->set_rules('username', 'Username', 'required|trim');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		if ($this->form_validation->run() == false) {
+			$this->load->view('login');
+		} else {
+			$this->process_login();
+		}
 	}
-	// 	$this->form_validation->set_rules('username', 'Username', 'required|trim');
-	// 	$this->form_validation->set_rules('password', 'Password', 'required|trim');
-	// 	if ($this->form_validation->run() == false) {
-	// 		$this->load->view('login');
-	// 	} else {
-	// 		$this->process_login();
-	// 	}
-	// }
 
-	// public function process_login()
-	// {
-	// 	$username = $this->input->post('username');
-	// 	$password = $this->input->post('password');
-	// 	$login = $this->db->get_where('user', ['username' => $username])->row_array();
-	// 	if ($login) {
-	// 		if (password_verify($password, $login['password'])) {
-	// 			$data = [
-	// 				'username' => $login['username'],
-	// 			];
-	// 			$this->session->set_userdata($data);
-	// 			redirect('AsianGames/viewcabor');
-	// 		} else {
-	// 			$this->session->set_flashdata('message', 'Gagal login: Cek password! ');
-	// 			redirect('AsianGames/login');
-	// 		}
-	// 	} else {
-	// 		$this->session->set_flashdata('message', 'Gagal login: Cek username! ');
-	// 		redirect('AsianGames/login');
-	// 	}
-    // }
+	public function process_login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$login = $this->db->get_where('user', ['username' => $username])->row_array();
+		if ($login) {
+			if (password_verify($password, $login['password'])) {
+				$data = [
+					'username' => $login['username'],
+				];
+				$this->session->set_userdata($data);
+				redirect('AsianGames/viewcabor');
+			} else {
+				$this->session->set_flashdata('message', 'Gagal login: Cek password! ');
+				redirect('AsianGames/login');
+			}
+		} else {
+			$this->session->set_flashdata('message', 'Gagal login: Cek username! ');
+			redirect('AsianGames/login');
+		}
+    }
+    
+    public function addAtlet(){
+        $this->load->view('insertAtlet');
+    }
 }
